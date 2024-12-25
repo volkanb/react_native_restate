c8b6dcd87adba6a1b6093e04d7397ed8b34cd6e3
volkanb <volkanbenli92@gmail.com>
2024-12-25 16:18:33 -0500

    Adding google sign in WIP

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,13 +1,27 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import images from '@/constants/images';
 import icons from '@/constants/icons';
+import { login } from '@/lib/appwrite';
 
 const SignIn = () => {
-  const handleLogin = () => {
-    // TODO: Implement Google login
+  const handleLogin = async () => {
+    const result = await login();
+
+    if (result) {
+      console.log('Login success');
+    } else {
+      Alert.alert('Error', 'Failed to login');
+    }
     console.log('Login pressed');
   };
 
